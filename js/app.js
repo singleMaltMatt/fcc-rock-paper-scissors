5aac2ce4d4cb2a2507a5389be6fbf8dd1acbc009
singleMaltMatt <mattmatte93@gmail.com>
2022-03-24 19:33:24 +0200

    scripting, added user and computer choice functions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
-const computerChoice = document.getElementById('computer-choice');
-const userChoice = document.getElementById('user-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 
+const possibleChoises = document.querySelectorAll('button');
+let userChoice
+let computerChoice
+
+    possibleChoises.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+        userChoice = e.target.id
+        userChoiceDisplay.innerHTML = userChoice
+        generateComputerChoice()
+    }));
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) // 3 or possibleChoises.length
+    // console.log(randomNumber);
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock';
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'scissors'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
